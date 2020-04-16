dfd0979c9aa97274cb55a854c6fff9c4c3486c6b
irahulcse <1rahulchandra1@gmail.com>
2020-04-17 05:07:24 +0530

    Automatic Commit 2020-04-17-05:07:22

---
+++ android/app/src/main/kotlin/com/example/instagroot_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.instagroot_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:outline_material_icons/outline_material_icons.dart';
+import 'package:instagroot_app_flutter/home_feed_page.dart';
+import 'package:instagroot_app_flutter/ui_utils.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(new MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagroot',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        primaryColor: Colors.black,
+      ),
+      home: MainScaffold(),
+    );
+  }
+}
+
+class MainScaffold extends StatefulWidget {
+  @override
+  _MainScaffoldState createState() => _MainScaffoldState();
+}
+
+class _MainScaffoldState extends State<MainScaffold> {
+  static const _kAddPhotoTabIndex = 2;
+  int _tabSelectedIndex = 0;
+
+  // Save the home page scrolling offset,
+  // used when navigating back to the home page from another tab.
+  double _lastFeedScrollOffset = 0;
+  ScrollController _scrollController;
+
+  @override
+  void dispose() {
+    _disposeScrollController();
+    super.dispose();
+  }
+
+  void _scrollToTop() {
+    if (_scrollController == null) {
+      return;
+    }
+    _scrollController.animateTo(
+      0.0,
+      duration: Duration(milliseconds: 250),
+      curve: Curves.decelerate,
+    );
+  }
+
+  // Call this when changing the body that doesn't use a ScrollController.
+  void _disposeScrollController() {
+    if (_scrollController != null) {
+      _lastFeedScrollOffset = _scrollController.offset;
+      _scrollController.dispose();
+      _scrollController = null;
+    }
+  }
+
+  void _onTabTapped(BuildContext context, int index) {
+    if (index == _kAddPhotoTabIndex) {
+      showSnackbar(context, 'Add Photo');
+    } else if (index == _tabSelectedIndex) {
+      _scrollToTop();
+    } else {
+      setState(() => _tabSelectedIndex = index);
+    }
+  }
+
+  Widget _buildPlaceHolderTab(String tabName) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 64.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Oops, the $tabName tab is\n under construction!',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 28.0),
+            ),
+            Image.asset('assets/images/building.gif'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    switch (_tabSelectedIndex) {
+      case 0:
+        _scrollController =
+            ScrollController(initialScrollOffset: _lastFeedScrollOffset);
+        return HomeFeedPage(scrollController: _scrollController);
+      default:
+        const tabIndexToNameMap = {
+          0: 'Home',
+          1: 'Search',
+          2: 'Add Photo',
+          3: 'Notifications',
+          4: 'Profile',
+        };
+        _disposeScrollController();
+        return _buildPlaceHolderTab(tabIndexToNameMap[_tabSelectedIndex]);
+    }
+  }
+
+  // Unselected tabs are outline icons, while the selected tab should be solid.
+  Widget _buildBottomNavigation() {
+    const unselectedIcons = <IconData>[
+      OMIcons.home,
+      Icons.search,
+      OMIcons.addBox,
+      Icons.favorite_border,
+      Icons.person_outline,
+    ];
+    const selecteedIcons = <IconData>[
+      Icons.home,
+      Icons.search,
+      Icons.add_box,
+      Icons.favorite,
+      Icons.person,
+    ];
+    final bottomNaivgationItems = List.generate(5, (int i) {
+      final iconData =
+          _tabSelectedIndex == i ? selecteedIcons[i] : unselectedIcons[i];
+      return BottomNavigationBarItem(icon: Icon(iconData), title: Container());
+    }).toList();
+
+    return Builder(builder: (BuildContext context) {
+      return BottomNavigationBar(
+        iconSize: 32.0,
+        type: BottomNavigationBarType.fixed,
+        items: bottomNaivgationItems,
+        currentIndex: _tabSelectedIndex,
+        onTap: (int i) => _onTabTapped(context, i),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1.0,
+        backgroundColor: Colors.grey[50],
+        title: Row(
+          children: <Widget>[
+            Builder(builder: (BuildContext context) {
+              return GestureDetector(
+                child: Icon(OMIcons.cameraAlt, color: Colors.black, size: 32.0),
+                onTap: () => showSnackbar(context, 'Add Photo'),
+              );
+            }),
+            SizedBox(width: 12.0),
+            GestureDetector(
+              child: Text(
+                'Instagroot',
+                style: TextStyle(
+                    fontFamily: 'Billabong',
+                    color: Colors.black,
+                    fontSize: 32.0),
+              ),
+              onTap: _scrollToTop,
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          Builder(builder: (BuildContext context) {
+            return IconButton(
+              color: Colors.black,
+              icon: Icon(OMIcons.liveTv),
+              onPressed: () => showSnackbar(context, 'Live TV'),
+            );
+          }),
+          Builder(builder: (BuildContext context) {
+            return IconButton(
+              color: Colors.black,
+              icon: Icon(OMIcons.nearMe),
+              onPressed: () => showSnackbar(context, 'My Messages'),
+            );
+          }),
+        ],
+      ),
+      body: _buildBody(),
+      bottomNavigationBar: _buildBottomNavigation(),
+    );
+  }
+}
